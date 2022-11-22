37f0f550e7103872b34edb6beed04e20558948b2
daniel-clover <daniel-clover@users.noreply.github.com>
2022-11-22 18:22:07 +0000

    Tokens updated

---
+++ build/ios-swift/StyleDictionary.swift
@@ -0,0 +1,118 @@
+
+//
+// StyleDictionary.swift
+//
+
+// Do not edit directly
+// Generated on Tue, 22 Nov 2022 18:22:06 GMT
+
+
+import UIKit
+
+public class StyleDictionary {
+    public static let betaSecondaryColorsBlueExtraDarkTextBeta = #212c31ff
+    public static let betaSecondaryColorsDeprecatedBlueDarkTextLinkBeta = #0764abff
+    public static let clover20WebBody = [object Object]
+    public static let clover20WebBodyBold = [object Object]
+    public static let clover20WebCaption = [object Object]
+    public static let clover20WebEyebrow = [object Object]
+    public static let clover20WebHeading1 = [object Object]
+    public static let clover20WebHeading2 = [object Object]
+    public static let clover20WebHeading3 = [object Object]
+    public static let clover20WebHeading4 = [object Object]
+    public static let clover20WebHeading5 = [object Object]
+    public static let clover20WebLabel = [object Object]
+    public static let clover20WebLink = [object Object]
+    public static let clover20WebMobileBody = [object Object]
+    public static let clover20WebMobileBodyBold = [object Object]
+    public static let clover20WebMobileCaption = [object Object]
+    public static let clover20WebMobileEyebrow = [object Object]
+    public static let clover20WebMobileHeading1 = [object Object]
+    public static let clover20WebMobileHeading2 = [object Object]
+    public static let clover20WebMobileHeading3 = [object Object]
+    public static let clover20WebMobileHeading4 = [object Object]
+    public static let clover20WebMobileHeading5 = [object Object]
+    public static let clover20WebMobileLabel = [object Object]
+    public static let clover20WebMobileLink = [object Object]
+    public static let clover20WebMobileSecondary = [object Object]
+    public static let clover20WebMobileSpotlight = [object Object]
+    public static let clover20WebMobileTertiaryButtonLabel = [object Object]
+    public static let clover20WebSecondary = [object Object]
+    public static let clover20WebSpotlight = [object Object]
+    public static let clover20WebTertiaryButtonLabel = [object Object]
+    public static let clover30Body = [object Object]
+    public static let clover30BodyBold = [object Object]
+    public static let clover30BodySmall = [object Object]
+    public static let clover30BodySmallBold = [object Object]
+    public static let clover30Caption = [object Object]
+    public static let clover30CaptionBold = [object Object]
+    public static let clover30Display1 = [object Object]
+    public static let clover30Heading1 = [object Object]
+    public static let clover30Heading2 = [object Object]
+    public static let clover30Heading3 = [object Object]
+    public static let clover30Heading4 = [object Object]
+    public static let clover30Label = [object Object]
+    public static let clover30Link = [object Object]
+    public static let colorBetaSecondaryColorsBlueExtraDarkTextBeta = UIColor(red: 0.129, green: 0.173, blue: 0.192, alpha: 1)
+    public static let colorBetaSecondaryColorsDeprecatedBlueDarkTextLinkBeta = UIColor(red: 0.027, green: 0.392, blue: 0.671, alpha: 1)
+    public static let colorNeutralColorsBlack = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 1)
+    public static let colorNeutralColorsGray = UIColor(red: 0.447, green: 0.447, blue: 0.447, alpha: 1)
+    public static let colorNeutralColorsGrayBackground = UIColor(red: 0.984, green: 0.984, blue: 0.984, alpha: 1)
+    public static let colorNeutralColorsGrayDark = UIColor(red: 0.361, green: 0.361, blue: 0.361, alpha: 1)
+    public static let colorNeutralColorsGrayDisabled = UIColor(red: 0.780, green: 0.780, blue: 0.780, alpha: 1)
+    public static let colorNeutralColorsGrayLight = UIColor(red: 0.898, green: 0.898, blue: 0.898, alpha: 1)
+    public static let colorNeutralColorsWhite = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1)
+    public static let colorPrimaryColorsGreen = UIColor(red: 0.133, green: 0.533, blue: 0.000, alpha: 1)
+    public static let colorPrimaryColorsGreenAccent = UIColor(red: 0.263, green: 0.690, blue: 0.165, alpha: 1)
+    public static let colorPrimaryColorsGreenBackground = UIColor(red: 0.949, green: 0.988, blue: 0.933, alpha: 1)
+    public static let colorPrimaryColorsGreenDark = UIColor(red: 0.153, green: 0.416, blue: 0.086, alpha: 1)
+    public static let colorSecondaryColorsBlue = UIColor(red: 0.196, green: 0.443, blue: 0.671, alpha: 1)
+    public static let colorSecondaryColorsBlueAccent = UIColor(red: 0.200, green: 0.616, blue: 1.000, alpha: 1)
+    public static let colorSecondaryColorsBlueBackground = UIColor(red: 0.902, green: 0.953, blue: 1.000, alpha: 1)
+    public static let colorSecondaryColorsBlueDark = UIColor(red: 0.157, green: 0.353, blue: 0.533, alpha: 1)
+    public static let colorSecondaryColorsBlueDarkBackground = UIColor(red: 0.145, green: 0.200, blue: 0.220, alpha: 1)
+    public static let colorSecondaryColorsBlueTextLink = UIColor(red: 0.027, green: 0.392, blue: 0.671, alpha: 1)
+    public static let colorTertiaryColorsEggplantAccent = UIColor(red: 0.600, green: 0.400, blue: 0.800, alpha: 1)
+    public static let colorTertiaryColorsEggplantBackground = UIColor(red: 0.980, green: 0.953, blue: 0.992, alpha: 1)
+    public static let colorTertiaryColorsEggplantDark = UIColor(red: 0.357, green: 0.243, blue: 0.467, alpha: 1)
+    public static let colorTertiaryColorsTeal = UIColor(red: 0.078, green: 0.506, blue: 0.553, alpha: 1)
+    public static let colorTertiaryColorsTealAccent = UIColor(red: 0.412, green: 0.816, blue: 0.867, alpha: 1)
+    public static let colorTertiaryColorsTealBackground = UIColor(red: 0.933, green: 0.984, blue: 0.988, alpha: 1)
+    public static let colorTertiaryColorsTealDark = UIColor(red: 0.157, green: 0.384, blue: 0.443, alpha: 1)
+    public static let colorUtilityColorsRed = UIColor(red: 0.906, green: 0.000, blue: 0.000, alpha: 1)
+    public static let colorUtilityColorsRedBackground = UIColor(red: 1.000, green: 0.929, blue: 0.929, alpha: 1)
+    public static let colorUtilityColorsRedDark = UIColor(red: 0.776, green: 0.000, blue: 0.000, alpha: 1)
+    public static let colorUtilityColorsRedPin = UIColor(red: 1.000, green: 0.369, blue: 0.369, alpha: 1)
+    public static let colorUtilityColorsYellow = UIColor(red: 1.000, green: 0.820, blue: 0.000, alpha: 1)
+    public static let colorUtilityColorsYellowBackground = UIColor(red: 1.000, green: 0.965, blue: 0.800, alpha: 1)
+    public static let neutralColorsBlack = #000000ff
+    public static let neutralColorsGray = #727272ff
+    public static let neutralColorsGrayBackground = #fbfbfbff
+    public static let neutralColorsGrayDark = #5c5c5cff
+    public static let neutralColorsGrayDisabled = #c7c7c7ff
+    public static let neutralColorsGrayLight = #e5e5e5ff
+    public static let neutralColorsWhite = #ffffffff
+    public static let primaryColorsGreen = #228800ff
+    public static let primaryColorsGreenAccent = #43b02aff
+    public static let primaryColorsGreenBackground = #f2fceeff
+    public static let primaryColorsGreenDark = #276a16ff
+    public static let secondaryColorsBlue = #3271abff
+    public static let secondaryColorsBlueAccent = #339dffff
+    public static let secondaryColorsBlueBackground = #e6f3ffff
+    public static let secondaryColorsBlueDark = #285a88ff
+    public static let secondaryColorsBlueDarkBackground = #253338ff
+    public static let secondaryColorsBlueTextLink = #0764abff
+    public static let tertiaryColorsEggplantAccent = #9966ccff
+    public static let tertiaryColorsEggplantBackground = #faf3fdff
+    public static let tertiaryColorsEggplantDark = #5b3e77ff
+    public static let tertiaryColorsTeal = #14818dff
+    public static let tertiaryColorsTealAccent = #69d0ddff
+    public static let tertiaryColorsTealBackground = #eefbfcff
+    public static let tertiaryColorsTealDark = #286271ff
+    public static let utilityColorsRed = #e70000ff
+    public static let utilityColorsRedBackground = #ffededff
+    public static let utilityColorsRedDark = #c60000ff
+    public static let utilityColorsRedPin = #ff5e5eff
+    public static let utilityColorsYellow = #ffd100ff
+    public static let utilityColorsYellowBackground = #fff6ccff
+}
