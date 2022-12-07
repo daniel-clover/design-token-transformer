08c67b2ee9bf4a3bef86e511a81f50b203dc0cb6
daniel-clover <daniel-clover@users.noreply.github.com>
2022-12-07 16:41:02 +0000

    Tokens updated

---
+++ build/ios/StyleDictionarySize.h
@@ -0,0 +1,52 @@
+
+// StyleDictionarySize.h
+//
+
+// Do not edit directly
+// Generated on Wed, 07 Dec 2022 16:41:01 GMT
+
+
+#import <Foundation/Foundation.h>
+
+
+extern float const TypographyClover30Display1FontWeight;
+extern float const TypographyClover30Heading1FontWeight;
+extern float const TypographyClover30Heading2FontWeight;
+extern float const TypographyClover30Heading3FontWeight;
+extern float const TypographyClover30Heading4FontWeight;
+extern float const TypographyClover30BodyFontWeight;
+extern float const TypographyClover30BodyBoldFontWeight;
+extern float const TypographyClover30LinkFontWeight;
+extern float const TypographyClover30LabelFontWeight;
+extern float const TypographyClover30BodySmallFontWeight;
+extern float const TypographyClover30BodySmallBoldFontWeight;
+extern float const TypographyClover30CaptionFontWeight;
+extern float const TypographyClover30CaptionBoldFontWeight;
+extern float const TypographyClover20WebHeading1FontWeight;
+extern float const TypographyClover20WebHeading2FontWeight;
+extern float const TypographyClover20WebHeading3FontWeight;
+extern float const TypographyClover20WebHeading4FontWeight;
+extern float const TypographyClover20WebHeading5FontWeight;
+extern float const TypographyClover20WebBodyFontWeight;
+extern float const TypographyClover20WebBodyBoldFontWeight;
+extern float const TypographyClover20WebLinkFontWeight;
+extern float const TypographyClover20WebLabelFontWeight;
+extern float const TypographyClover20WebSecondaryFontWeight;
+extern float const TypographyClover20WebCaptionFontWeight;
+extern float const TypographyClover20WebTertiaryButtonLabelFontWeight;
+extern float const TypographyClover20WebEyebrowFontWeight;
+extern float const TypographyClover20WebSpotlightFontWeight;
+extern float const TypographyClover20WebMobileHeading1FontWeight;
+extern float const TypographyClover20WebMobileHeading2FontWeight;
+extern float const TypographyClover20WebMobileHeading3FontWeight;
+extern float const TypographyClover20WebMobileHeading4FontWeight;
+extern float const TypographyClover20WebMobileHeading5FontWeight;
+extern float const TypographyClover20WebMobileBodyFontWeight;
+extern float const TypographyClover20WebMobileBodyBoldFontWeight;
+extern float const TypographyClover20WebMobileLinkFontWeight;
+extern float const TypographyClover20WebMobileLabelFontWeight;
+extern float const TypographyClover20WebMobileSecondaryFontWeight;
+extern float const TypographyClover20WebMobileCaptionFontWeight;
+extern float const TypographyClover20WebMobileTertiaryButtonLabelFontWeight;
+extern float const TypographyClover20WebMobileEyebrowFontWeight;
+extern float const TypographyClover20WebMobileSpotlightFontWeight;
