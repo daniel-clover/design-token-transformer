3f984cb31dfa125a0648f67af9991404f651b532
daniel-clover <daniel-clover@users.noreply.github.com>
2022-12-15 21:19:22 +0000

    Tokens updated

---
+++ build/ios-swift/StyleDictionarySize.swift
@@ -0,0 +1,30 @@
+
+//
+// StyleDictionarySize.swift
+//
+
+// Do not edit directly
+// Generated on Thu, 15 Dec 2022 21:19:22 GMT
+
+
+import UIKit
+
+public enum StyleDictionarySize {
+    public static let bodyBoldFontWeight = 500
+    public static let bodyFontWeight = 400
+    public static let bodySmallBoldFontWeight = 500
+    public static let bodySmallFontWeight = 400
+    public static let display1FontWeight = 600
+    public static let display2FontWeight = 600
+    public static let dynamicDisplay1LargeFontWeight = 600
+    public static let dynamicDisplay1MediumFontWeight = 600
+    public static let dynamicDisplay2MediumFontWeight = 600
+    public static let heading1FontWeight = 600
+    public static let heading2FontWeight = 600
+    public static let heading3FontWeight = 500
+    public static let heading4FontWeight = 500
+    public static let helperTextFontWeight = 400
+    public static let labelFontWeight = 500
+    public static let legalFontWeight = 400
+    public static let linkFontWeight = 400
+}
---
+++ build/ios/StyleDictionarySize.m
@@ -0,0 +1,29 @@
+
+//
+// StyleDictionarySize.m
+//
+
+// Do not edit directly
+// Generated on Thu, 15 Dec 2022 21:19:22 GMT
+
+
+#import "StyleDictionarySize.h"
+
+
+float const TypographyDisplay1FontWeight = 600;
+float const TypographyDisplay2FontWeight = 600;
+float const TypographyHeading1FontWeight = 600;
+float const TypographyHeading2FontWeight = 600;
+float const TypographyHeading3FontWeight = 500;
+float const TypographyHeading4FontWeight = 500;
+float const TypographyBodyFontWeight = 400;
+float const TypographyBodyBoldFontWeight = 500;
+float const TypographyBodySmallFontWeight = 400;
+float const TypographyBodySmallBoldFontWeight = 500;
+float const TypographyLinkFontWeight = 400;
+float const TypographyLabelFontWeight = 500;
+float const TypographyHelperTextFontWeight = 400;
+float const TypographyLegalFontWeight = 400;
+float const TypographyDynamicDisplay1LargeFontWeight = 600;
+float const TypographyDynamicDisplay1MediumFontWeight = 600;
+float const TypographyDynamicDisplay2MediumFontWeight = 600;
