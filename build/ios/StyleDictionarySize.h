d445b86ddbefb1a7e0e7bb4c6011d319ddd8feb7
daniel-clover <daniel-clover@users.noreply.github.com>
2022-12-16 17:02:53 +0000

    Tokens updated

---
+++ build/ios/StyleDictionarySize.h
@@ -0,0 +1,28 @@
+
+// StyleDictionarySize.h
+//
+
+// Do not edit directly
+// Generated on Fri, 16 Dec 2022 17:02:53 GMT
+
+
+#import <Foundation/Foundation.h>
+
+
+extern float const TypographyDisplay1FontWeight;
+extern float const TypographyDisplay2FontWeight;
+extern float const TypographyHeading1FontWeight;
+extern float const TypographyHeading2FontWeight;
+extern float const TypographyHeading3FontWeight;
+extern float const TypographyHeading4FontWeight;
+extern float const TypographyBodyFontWeight;
+extern float const TypographyBodyBoldFontWeight;
+extern float const TypographyBodySmallFontWeight;
+extern float const TypographyBodySmallBoldFontWeight;
+extern float const TypographyLinkFontWeight;
+extern float const TypographyLabelFontWeight;
+extern float const TypographyHelperTextFontWeight;
+extern float const TypographyLegalFontWeight;
+extern float const TypographyDynamicDisplay1LargeFontWeight;
+extern float const TypographyDynamicDisplay1MediumFontWeight;
+extern float const TypographyDynamicDisplay2MediumFontWeight;
