17cf7c1d507bc055773aeca726a5c55ae78436f9
daniel-clover <daniel-clover@users.noreply.github.com>
2022-12-20 19:41:39 +0000

    Tokens updated

---
+++ build/ios/StyleDictionarySize.h
@@ -0,0 +1,25 @@
+
+// StyleDictionarySize.h
+//
+
+// Do not edit directly
+// Generated on Tue, 20 Dec 2022 19:41:39 GMT
+
+
+#import <Foundation/Foundation.h>
+
+
+extern float const TypographyClover30Heading1FontWeight;
+extern float const TypographyClover30Heading2FontWeight;
+extern float const TypographyClover30Heading3FontWeight;
+extern float const TypographyClover30Heading4FontWeight;
+extern float const TypographyClover30BodyFontWeight;
+extern float const TypographyClover30BodySmallFontWeight;
+extern float const TypographyClover30BodyBoldFontWeight;
+extern float const TypographyClover30BodySmallBoldFontWeight;
+extern float const TypographyClover30LinkFontWeight;
+extern float const TypographyClover30LabelFontWeight;
+extern float const TypographyClover30CaptionFontWeight;
+extern float const TypographyClover30CaptionBoldFontWeight;
+extern float const TypographyClover30Heading6LargeFontWeight;
+extern float const TypographyClover30Heading6MediumFontWeight;
